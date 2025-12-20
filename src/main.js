501e530c9401a42f46673c93e0ae8238025935f3
MuhammadAmadi <type.92@mail.ru>
2025-12-20 23:10:23 +0300

    error correction 13

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
    const { discount , sale_price, quantity } = purchase;
 
-   return +(sale_price * quantity * (1 - discount / 100)).toFixed(2);
+   return +(sale_price * quantity * (1 - discount / 100)).toFixed(3);
 }
 
 /**

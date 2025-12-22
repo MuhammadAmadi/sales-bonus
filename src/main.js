9d90a384e9b823badf35c43490203272d01c6422
MuhammadAmadi <type.92@mail.ru>
2025-12-22 12:14:03 +0300

    error correction 19

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
    const { discount , sale_price, quantity } = purchase;
 
-   return sale_price * quantity * (1 - discount / 100);
+   return _product * quantity * (1 - discount / 100);
 }
 
 /**
@@ -85,14 +85,26 @@ function analyzeSalesData(data, options) {
     data.purchase_records.forEach( record => {
         const seller = sellerIndex[record.seller_id];
         seller.sales_count += 1;
+        seller.revenue += record.total_amount;
+
         record.items.forEach( item => {
-            const sku = item.sku;
-            seller.products_sold[sku] = (seller.products_sold[sku] || 0) + item.quantity;
+            const product = productIndex[item.sku];
+            console.log("product ",product);
+            const cost = product.purchase_price * item.quantity;
+            const revenue = calculateRevenue(item, product);
+            const profit = revenue - cost;
+
+            seller.profit += profit;
+        })
+
+        // record.items.forEach( item => {
+        //     const sku = item.sku;
+        //     seller.products_sold[sku] = (seller.products_sold[sku] || 0) + item.quantity;
             
-            const revenue = calculateRevenue(item);
-            seller.revenue += +(revenue).toFixed(2);
-            seller.profit += revenue - (productIndex[sku] * item.quantity);
-        });
+        //     const revenue = calculateRevenue(item);
+        //     seller.revenue += +(revenue).toFixed(2);
+        //     seller.profit += revenue - (productIndex[sku] * item.quantity);
+        // });
 
         seller.revenue -= record.total_discount;
     });
